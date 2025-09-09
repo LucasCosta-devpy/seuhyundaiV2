49473d12184d1d85ce8e33c24bfcb9c73453d94e
LUCAS COSTA <lucas_dscosta@hotmail.com>
2025-09-09 12:01:55 -0300

    Configurar GitHub Pages com GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { resolve } from 'path'
 export default defineConfig({
   plugins: [react()],
   // Configuração condicional para desenvolvimento e produção
-  base: process.env.NODE_ENV === 'production' ? '/seuhyundai/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/seuhyundaiteste/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
